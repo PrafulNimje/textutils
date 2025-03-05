e6872b6e69db087bedc55280e758b0bc6f98bcf0
prafulnimje <prafulnimje1999@gmail.com>
2025-03-05 16:41:38 +0530

    navbar dark mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 
+import { useState } from 'react';
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
-function App() {
+function App() {  
+  const [mode,setMode] = useState('light')
+
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+    }
+    else{
+      setMode('light')
+    }
+  }
   return (
     <>
-    <Navbar title="TextUtils" aboutText="About TextUtils"/>
-    <div className="container m-6">
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+    <div className="container m-6 my-3">
       <TextForm heading="Enter text to analyze below"></TextForm>
+      {/* <About></About> */}
     </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,14 +1,35 @@
 import React, {useState}from 'react'
 
 export default function TextForm() {
-    const[text,setText] = useState('Enter text here')
+    const[text,setText] = useState('')
 
     const handleUpClick=()=>{
         console.log("Uppercase is clicked " + text)
         let newText = text.toUpperCase();
         setText(newText)
     }
+    const handleLoClick=()=>{
+        console.log("Uppercase is clicked " + text)
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+    const handleClearClick=()=>{
+        console.log("Uppercase is clicked " + text)
+        let newText = "";
+        setText(newText)
+    }
 
+    const handleCopy=()=>{
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+
+    }
+
+    const handleExtraSpace=()=>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(' '))
+    }
     const handleOnClick=(event)=>{
         console.log("On change")
         setText(event.target.value)
@@ -21,10 +42,20 @@ export default function TextForm() {
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnClick} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick} >Convert to Uppercase</button>
+            <button className="btn btn-primary mx-1" onClick={handleUpClick} >Convert to Uppercase</button>
+            <button className="btn btn-primary mx-1" onClick={handleLoClick} >Convert to Lowercase</button>
+            <button className="btn btn-primary mx-1" onClick={handleClearClick} >Clear Text</button>
+            <button className="btn btn-primary mx-1" onClick={handleCopy} >Copy Text</button>
+            <button className="btn btn-primary mx-1" onClick={handleExtraSpace} >Remove Extra Spaces</button>
+
         </div>
         <div className="container my-3">
             <h2>Your Text Summary</h2>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{(0.008*text.split(" ").length).toFixed(3)} Minutes read</p>
+            <p>{text.split(/\r\n|\r|\n/).length} Lines</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
         </div>
         </>
     )
